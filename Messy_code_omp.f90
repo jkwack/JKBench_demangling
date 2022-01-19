ab20fcfb947901e7cbf1144634bd78ed070a78b7
JaeHyuk Kwack <jkwack@anl.gov>
2022-01-19 21:56:00 +0000

    Add another module

--- Messy_code_omp.f90
+++ Messy_code_omp.f90
@@ -3,16 +3,50 @@
 !
 !                               written by JaeHyuk Kwack (jkwack@anl.gov)
 ! ------------------------------------------------------------------------------
-module JK_shared
-   use omp_lib
-   implicit none
+module JK_sub_shared
+  use omp_lib
+  implicit none
 
-
-   type::data_type
+  type::data_type
       integer                                   :: n
       real(kind=8),dimension(:),allocatable     :: DP
       real(kind=4),dimension(:),allocatable     :: SP
    end type data_type
+
+   contains
+
+   subroutine alloc_data(this_data,n)
+      integer,intent(in)                       :: n
+      type(data_type),intent(inout)            :: this_data
+      real(kind=8),dimension(:),allocatable    :: A
+      integer                                  :: i,j,k
+
+      this_data%n = n;
+      allocate(this_data%DP(n));
+      allocate(this_data%SP(n));
+
+      allocate(A(n))
+      !$omp target teams distribute parallel do map(to:A)
+         do i=1,n
+            A(i) = i*100.0
+            do j=1,i
+               A(i) = A(i) + j
+            enddo
+         enddo
+      !$omp end target teams distribute parallel do
+
+   end subroutine alloc_data
+
+end module JK_sub_shared
+
+
+
+
+
+module JK_shared_with_a_long_name
+   use omp_lib
+   use JK_sub_shared
+   implicit none
 
 
    type::L_type
@@ -33,17 +67,17 @@ module JK_shared
 
       k = n
 
-      do i=1,n
+      do i=1,n*100
         do j=1,i
-           do k=1,n
+           do k=1,n*100
               n = n+i+j+k
            enddo
         enddo
       enddo
 
-      do k=1,n
+      do k=1,n*100
         do j=1,k
-           do i=1,n
+           do i=1,n*100
               n = n-k-i-j
            enddo
         enddo
@@ -53,16 +87,6 @@ module JK_shared
    end subroutine use_gpu_task_B
 
 
-
-   subroutine alloc_data(this_data,n)
-      integer,intent(in)                       :: n
-      type(data_type),intent(inout)            :: this_data
-
-      this_data%n = n;
-      allocate(this_data%DP(n));
-      allocate(this_data%SP(n));
-
-   end subroutine alloc_data
 
    subroutine use_gpu_task_A(n)
       integer,intent(in)                       :: n
@@ -80,17 +104,19 @@ module JK_shared
       !$omp end target teams distribute parallel do
 
       k=n
-      !$omp target 
+      !$omp target teams 
          call use_gpu_task_B(k)
-      !$omp end target
+      !$omp end target teams
 
-      !$omp target teams distribute parallel do map(to:A)
+      !$omp target map(to:A)
+      !$omp teams distribute parallel do
          do i=1,n
             do j=i,n
                A(i) = A(i) - j
             enddo
          enddo
-      !$omp end target teams distribute parallel do
+      !$omp end teams distribute parallel do
+      !$omp end target
 
    end subroutine use_gpu_task_A
 
@@ -101,13 +127,10 @@ module JK_shared
       real(kind=8),dimension(:),allocatable    :: tmp
 
       n = this_data%n
-      allocate(tmp(n))
-
-      !$omp target enter data map (to:this_data)
+      allocate(tmp(n*n))
 
       !$omp target teams distribute parallel do map(tmp)
-         do i=1,n
-            !this_data%DP(i) = i*100.0
+         do i=1,n*n
             tmp(i) = i*100.0
             do j=1,i
                tmp(i) = tmp(i) + j
@@ -115,7 +138,9 @@ module JK_shared
          enddo 
       !$omp end target teams distribute parallel do
 
-      call use_gpu_task_A(n)
+      do i=1,n
+         call use_gpu_task_A(n)
+      enddo
 
    end subroutine init_data
 
@@ -132,10 +157,8 @@ module JK_shared
       
       write(*,'(5x,A40,I6)') 'Initializing a level for ',level
 
-      write(*,'(30x,A)') '- allocating array for the level'
       call alloc_data(this_level%D,level*100)
 
-      write(*,'(30x,A)') '- initializing array for the level'
       call init_data(this_level%D)
 
       if (level .gt. 1) then
@@ -157,11 +180,11 @@ module JK_shared
 
 
 
-end module JK_shared
+end module JK_shared_with_a_long_name
 
 
 program main
-   use JK_shared
+   use JK_shared_with_a_long_name
    implicit none
 
    character(len=32)                            :: arg
